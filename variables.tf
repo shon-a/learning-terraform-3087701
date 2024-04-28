a5147103fd2f46e189631ebce1b296585380d2fa
Shon <158628894+shon-a@users.noreply.github.com>
2024-04-27 22:53:59 -0400

    Update variables.tf
    added access key variables

--- variables.tf
+++ variables.tf
@@ -2,3 +2,12 @@
 #  description = "Type of EC2 instance to provision"
 #  default     = "t3.nano"
 #}
+variable "AWS_ACCESS_KEY_ID" {
+  type         = string
+  description  = "AWS Access Key ID"
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  type        = string
+  description = "AWS Secrety Access Key"
+}
