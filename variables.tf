fb041b6d2b7a590761d85c46fd7f6890ff54f76b
Shon <158628894+shon-a@users.noreply.github.com>
2024-04-28 00:14:18 -0400

    Initial commit

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
-#variable "instance_type" {
-#  description = "Type of EC2 instance to provision"
-#  default     = "t3.nano"
-#}
+variable "instance_type" {
+  description = "Type of EC2 instance to provision"
+  default     = "t3.nano"
+}
 variable "AWS_ACCESS_KEY_ID" {
   type         = string
   description  = "AWS Access Key ID"
